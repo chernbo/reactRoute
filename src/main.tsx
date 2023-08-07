9cb2e19be3dbbf3084d649e977183da7147609c7
chenbo <chenbo@itiger.com>
2023-08-07 19:58:42 +0800

    feat(master): 添加路由操作

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,8 @@ import ErrorPage from './ErrorPage'
 import Contact, { loader as contactLoader } from './routes/Contact'
 import { loader as rootLoader, action as rootAction } from './routes/Root'
 import EditContact, { action as editAction } from './routes/edit'
-
+import { action as destroyAction } from './routes/destroy'
+import Index from './routes'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      {
+        index: true,
+        element: <Index />
+      },
       {
         path: "contacts/:contactId",
         element: <Contact />,
@@ -30,6 +35,12 @@ const router = createBrowserRouter([
         // 这里作者是偷懒，路由之间不共享加载器
         loader: contactLoader,
         action: editAction
+      },
+      {
+        path: 'contacts/:contactId/destroy',
+        action: destroyAction,
+        // 如果不写errorElement 会冒泡到最近的errorElement,(根路由上面的errorElement)
+        errorElement: <div>Oops! There was an error.</div>,
       }
     ]
   },
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,12 +1,15 @@
-import { Form, Link, Outlet, redirect, useLoaderData } from "react-router-dom";
+import { Form, Link, NavLink, Outlet, redirect, useLoaderData, useNavigate, useNavigation } from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
+import { useEffect } from "react";
 
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function loader({ request }) {
+  const url = new URL(request.url)
+  const query = url.searchParams.get('q')
+  const contacts = await getContacts(query);
+  return { contacts, query };
 }
-// 路由Form 提交，触发 action;
+// 路由Form 提交，post 请求会 触发 action;
 export async function action() {
   const contact = await createContact();
   return redirect(`/contacts/${contact.id}/edit`);
@@ -14,20 +17,27 @@ export async function action() {
 
 export default function Root() {
 
-  const { contacts } = useLoaderData();
+  const { contacts, query } = useLoaderData();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    document.getElementById("q").value = query;
+  }, [query]);
 
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
-        <div>
-          <form id="search-form" role="search">
+        <div >
+          {/* 客户端路由提交此表单 */}
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={query}
             />
             <div
               id="search-spinner"
@@ -38,7 +48,7 @@ export default function Root() {
               className="sr-only"
               aria-live="polite"
             ></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
@@ -48,7 +58,9 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  {/* <Link to={`contacts/${contact.id}`}
+
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -57,7 +69,20 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </Link> */}
+                  <NavLink to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "isPending" : ""}
+                  >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -68,7 +93,10 @@ export default function Root() {
           )}
         </nav>
       </div >
-      <div id="detail">
+      {/* 短暂延迟后添加一个漂亮的淡入淡出（以避免快速加载时闪烁 UI） */}
+      <div id="detail"
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         {/* 父级中，呈现子路由组件 */}
         <Outlet />
       </div>
